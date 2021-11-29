a72a55ca747e8ae7c6d8d915155cb832dffa2f77
nathanandre <nathanandreburns@gmail.com>
2021-11-28 21:07:43 -0500

    created the form for user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
         erb :login 
     end 
 
+    # This route receives login form ten finds user and logs the user in (create a session). 
+    post '/login' do 
+
+    end 
+
     get '/signup' do 
     end 
      
