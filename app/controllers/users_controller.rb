0ca1d8776fa574397b4692ad41b41825b74b48c7
nathanandre <nathanandreburns@gmail.com>
2021-11-30 23:32:54 -0500

    updated the post login route to log in a user. display user show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,20 @@ class UsersController < ApplicationController
 
     # This route receives login form ten finds user and logs the user in (create a session). 
     post '/login' do 
+        @user = User.find_by(email: params[:email]) 
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "users/#{@user.id}"
+        else
 
+        end 
     end 
 
     get '/signup' do 
+    end 
+
+    get '/users/:id' do 
+        "this will be the user show route"
     end 
      
 end 
