8ced3d9036f0aba30910d239baafd75412af6935
nathanandre <nathanandreburns@gmail.com>
2021-12-02 20:48:12 -0500

    updated sign up route and post sign up also signup erb form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
-    get '/login' do 
-        erb :login 
+    get '/login' do # <- get login, is going to load my 'login' page. 
+        erb :login  #<- render the login form view where credentials are submitted. 
     end 
 
-    # This route receives login form ten finds user and logs the user in (create a session). 
+    # This route receives login form then finds user and logs the user in (create a session). 
     post '/login' do 
-        @user = User.find_by(email: params[:email]) 
-        if @user.authenticate(params[:password])
-            session[:user_id] = @user.id
+        @user = User.find_by(email: params[:email]) # <- here we find the user using the login, i.e, email 
+        if @user.authenticate(params[:password]) # <- here we authenticate that the email/password combination is correct. 
+            session[:user_id] = @user.id # <- this actually logs in and assigns a key value pair to the session hash. 
             puts session 
-            redirect "users/#{@user.id}"
+            redirect "users/#{@user.id}" # <- show page (if we login we then redirect to the users show page.)
         else
 
         end 
     end 
 
+    # this route renders the sign up form 
     get '/signup' do 
+        erb :signup 
+    end 
+
+    # here is where we will create a new user and persist them to the Database. 
+    post '/users' do 
+
     end 
 
     get '/users/:id' do 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
-    has_secure_password 
+    has_secure_password #<- allows us to user the active record method 'authenticate'
 
 
     has_many :portfolios   
