7c97291ca0e9f3336d260384865cc2d11feefe62
nathanandre <nathanandreburns@gmail.com>
2021-12-04 19:06:36 -0500

    updated the post route company_stocks

--- app/controllers/company_stocks_controller.rb
+++ app/controllers/company_stocks_controller.rb
@@ -5,7 +5,16 @@ class CompanyStocksController < ApplicationController
         erb :'/company_stocks/new' #<- render form to create a new stock. form nestled in 'new' under the company_stocks folder in views 
     end 
 
-    post '/company_stocks' do 
-        
+    post '/company_stocks' do #<- create new journal entry. saves it to the data base. 
+        if !logged_in?
+            redirect '/'
+        end 
+        if params[:name] && params[:ticker] && params[:description] != "" #<- if these fields in the new stock form are not empty then we will create a new company stocks using the params
+            @company_stock = CompanyStock.create(name: params[:name], ticker: params[:ticker], description: params[:description], user_id: current_user.id) #<- creating a new instance of a stock and saving it under the instance variable @company_stock 
+            redirect "/company_stock/#{@company_stock.id}"
+        else 
+            redirect '/company_stock/new'
+        end 
+
     end 
 end 
